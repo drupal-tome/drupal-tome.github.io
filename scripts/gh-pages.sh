e93c386439d4d62327f4a12bc4a5ba593ab50364
Samuel Mortenson <samuel@mortenson.coffee>
2018-06-17 19:33:06 -0700

    Updated .gitignore.

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 rm -rf gh-pages
-git clone git@github.com:drupal-tome/drupal-tome.github.io.git master gh-pages
+git clone git@github.com:drupal-tome/drupal-tome.github.io.git -b master gh-pages
 rm -rf gh-pages/*
 cp -r html/* gh-pages/
 cd gh-pages
-git commit -am 'Updating gh-pages site'
-git push
+git add .
+git commit -m 'Updating gh-pages site'
+git push -u origin master
