87ca72b53ed866fd613337160039420e169dba15
Samuel Mortenson <samuel@mortenson.coffee>
2018-08-05 21:28:22 -0700

    Finished Github pages documentation.

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 drush cron -l https://tome.fyi
 drush tome:static -l https://tome.fyi
 rm -rf gh-pages
-git clone git@github.com:drupal-tome/drupal-tome.github.io.git -b master gh-pages
+git clone git@github.com:drupal-tome/drupal-tome.github.io.git gh-pages
+cd gh-pages
+git checkout -B master
+cd ..
 rm -rf gh-pages/*
 cp -r html/* gh-pages/
 cp CNAME gh-pages/CNAME
