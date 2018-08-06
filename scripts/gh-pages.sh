ac8ce84dbc3074c203b0384732fd15c1050a009c
Samuel Mortenson <samuel@mortenson.coffee>
2018-08-05 21:39:03 -0700

    Use more resilient branch create/checkout method.

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -7,7 +7,7 @@ drush tome:static -l https://tome.fyi
 rm -rf gh-pages
 git clone git@github.com:drupal-tome/drupal-tome.github.io.git gh-pages
 cd gh-pages
-git checkout -B master
+git checkout master || git checkout -b master
 cd ..
 rm -rf gh-pages/*
 cp -r html/* gh-pages/
