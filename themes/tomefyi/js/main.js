4d34fd42fb896382c6521716002d98102ac3b998
Samuel Mortenson <samuel@mortenson.coffee>
2018-09-08 13:03:37 -0700

    Updated homepage.

--- themes/tomefyi/js/main.js
+++ themes/tomefyi/js/main.js
@@ -1,5 +1,20 @@
 (function ($, Drupal) {
 
+  // Source: https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+  function isScrolledIntoView (elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  $(window).on('scroll', function () {
+    $('#coworkers').each(function () {
+      $(this).toggleClass('in-view', isScrolledIntoView($(this)));
+    });
+  });
+
   Drupal.behaviors.tomeFyiBurger = {
     attach: function attach(context, settings) {
       $('.burger', context).once('tome-burger').on('click', function () {
