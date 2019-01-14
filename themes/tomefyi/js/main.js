42f162da1f3772435af446a69a559d9313729ed4
Samuel Mortenson <samuel@mortenson.coffee>
2019-01-13 16:38:22 -0800

    ISA

--- themes/tomefyi/js/main.js
+++ themes/tomefyi/js/main.js
@@ -1,24 +1,17 @@
 (function ($, Drupal) {
 
-  // Source: https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
-  function isScrolledIntoView (elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  }
-
-  $(window).on('scroll', function () {
-    $('#coworkers').each(function () {
-      $(this).toggleClass('in-view', isScrolledIntoView($(this)));
-    });
-  });
-
   Drupal.behaviors.tomeFyiBurger = {
     attach: function attach(context, settings) {
       $('.burger', context).once('tome-burger').on('click', function () {
         $(this).siblings('.burger-menu').toggleClass('expanded');
+      });
+    }
+  };
+
+  Drupal.behaviors.tomeFyiProxyClick = {
+    attach: function attach(context, settings) {
+      $('[data-proxy-click]', context).once('tome-proxy-click').on('click', function () {
+        $(this).find('[data-proxy-click-target] a')[0].click();
       });
     }
   };
